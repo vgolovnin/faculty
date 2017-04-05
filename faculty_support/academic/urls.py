458da08928d2726987d49168b4d777129a10556f
Victor Golovnin <victor.golovnin@gmail.com>
2017-04-05 22:32:17 +0300

    Ami report url

--- faculty_support/academic/urls.py
+++ faculty_support/academic/urls.py
@@ -12,7 +12,8 @@ router.register(r'reserve', ReservistsViewSet, base_name='reservist')
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^api/', include(router.urls)),
-    url(r'^reports/dean$', reports.make),
-    url(r'^reports/stage/(?P<stage_id>[0-9]+)$', reports.make)
+    url(r'^reports/dean$', reports.make, {'step_id': 10}),
+    url(r'^reports/ami$', reports.make, {'step_id': 7}),
+    url(r'^reports/step/(?P<step_id>[0-9]+)$', reports.make)
 ]
 
