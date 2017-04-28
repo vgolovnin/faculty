8a7ee573d11bbb56db9299fd31ed2c6c49632b0e
Victor Golovnin <victor.golovnin@gmail.com>
2017-04-28 04:36:28 +0300

    vuejs router

--- academic/assets/index.js
+++ academic/assets/index.js
@@ -1,39 +1,24 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
 import Cookies from 'js-cookie'
 import _ from 'lodash'
 
 import './css/main.css'
 
-
+Vue.use(VueRouter)
 Vue.use(VueResource)
 Vue.http.headers.common['X-CSRFToken'] = Cookies.get('csrftoken');
 
 import Reserve from './vue/Reserve.vue'
 import Reports from './vue/Reports.vue'
 
-const routes = {
-    '/' : Reserve,
-    '/reports/': Reports,
-}
 
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+    {path: '/', component: Reserve},
+    {path: '/reports/', component: Reports},
+]})
 
-
-var vm = new Vue({
-    el: '#app',
-    data: {
-        currentRoute: window.location.pathname,
-    },
-    computed: {
-        ViewComponent() {
-            return routes[this.currentRoute] || null
-        },
-    },
-  render(h) { return h(this.ViewComponent); }
-})
-
-
-window.addEventListener('popstate', () => {
-  vm.currentRoute = window.location.pathname
-})
-
+new Vue({router}).$mount('#fs')
