86a56881c208fd1119d320e017c1301f6d89bfe9
Victor Golovnin <victor.golovnin@gmail.com>
2017-09-24 23:47:19 +0300

    fix calendar

--- academic/assets/admin/academic_admin.js
+++ academic/assets/admin/academic_admin.js
@@ -1,17 +1,19 @@
 (function($){
     function togglePhd(){
         const phd = $("#id_phd").parent();
-        if ($("#id_degree").val() === "Нет")
-        {
-            phd.hide();
-            phd.parent().find(".errorlist").hide();
-        }
-        else
-        {
-            phd.show();
-            const calendarnum = phd.find("a[id^=calendarlink]").attr("id").substr(12);
-            DateTimeShortcuts.openCalendar(calendarnum);
-        }
+        try {
+            if ($("#id_degree").val() === "Нет")
+            {
+                phd.hide();
+                phd.parent().find(".errorlist").hide();
+            }
+            else
+            {
+                phd.show();
+                const calendarnum = phd.find(".date-icon").parent().attr("id").substr(12);
+                DateTimeShortcuts.openCalendar(calendarnum);
+            }
+        } catch (err) {}
     }
     $(function(){
         $("#id_degree").change(togglePhd);
