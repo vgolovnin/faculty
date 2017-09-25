cc3a5fbc2ff6865455330470f9be41d0d34a27b4
Victor Golovnin <victor.golovnin@gmail.com>
2017-09-25 04:47:50 +0300

    set app url

--- webpack.config.js
+++ webpack.config.js
@@ -75,7 +75,7 @@ module.exports = {
         new webpack.DefinePlugin({
             'process.env': {
                 NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-                APP_URL: process.env.NODE_ENV === 'production' ? `'/faculty/'` : `'/'`,
+                APP_URL: `'/'`,
             }
         }),
     ],
